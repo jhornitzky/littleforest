26758b1414c2d06f779fc34f0456479cc7d65083
James Hornitzky <james.hornitzky@gmail.com>
2017-04-03 22:08:41 +1000

    New styling, working delete

--- index.php
+++ index.php
@@ -7,52 +7,30 @@
 <body>
     <section class="todoapp">
       <header class="header">
-        <h1>todos</h1>
+        <h1>littleforest</h1>
         <input class="new-todo"
           autofocus autocomplete="off"
-          placeholder="What needs to be done?"
+          placeholder="today is a lovely day"
           v-model="newTodo"
           @keyup.enter="addTodo">
       </header>
       <section class="main" v-show="todos.length" v-cloak>
-        <input class="toggle-all" type="checkbox" v-model="allDone">
         <ul class="todo-list">
           <li v-for="todo in filteredTodos"
             class="todo"
-            :key="todo.id"
-            :class="{ completed: todo.completed, editing: todo == editedTodo }">
-            <div class="view">
-              <input class="toggle" type="checkbox" v-model="todo.completed">
-              <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
-              <button class="destroy" @click="removeTodo(todo)"></button>
-            </div>
+            :key="todo.id">
             <input class="edit" type="text"
               v-model="todo.title"
               v-todo-focus="todo == editedTodo"
               @blur="doneEdit(todo)"
+              @keydown.delete="checkForDelete(todo)"
               @keyup.enter="doneEdit(todo)"
               @keyup.esc="cancelEdit(todo)">
           </li>
         </ul>
       </section>
-      <footer class="footer" v-show="todos.length" v-cloak>
-        <span class="todo-count">
-          <strong>{{ remaining }}</strong> {{ remaining | pluralize }} left
-        </span>
-        <ul class="filters">
-          <li><a href="#/all" :class="{ selected: visibility == 'all' }">All</a></li>
-          <li><a href="#/active" :class="{ selected: visibility == 'active' }">Active</a></li>
-          <li><a href="#/completed" :class="{ selected: visibility == 'completed' }">Completed</a></li>
-        </ul>
-        <button class="clear-completed" @click="removeCompleted" v-show="todos.length > remaining">
-          Clear completed
-        </button>
-      </footer>
     </section>
     <footer class="info">
-      <p>Double-click to edit a todo</p>
-      <p>Written by <a href="http://evanyou.me">Evan You</a></p>
-      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
     </footer>
 
     <!-- scripts -->
@@ -96,6 +74,14 @@
             }
           },
 
+          // computed properties
+          // http://vuejs.org/guide/computed.html
+          computed: {
+            filteredTodos: function () {
+              return this.todos
+            },
+          },
+
           // note there's no DOM manipulation here at all.
           methods: {
             addTodo: function () {
@@ -103,12 +89,15 @@
               if (!value) {
                 return
               }
-              this.todos.push({
+              this.todos.splice(0, 0,{
                 id: todoStorage.uid++,
                 title: value,
                 completed: false
               })
               this.newTodo = ''
+              Vue.nextTick(function() {
+                this.todos[10].$els.inputElement.focus();
+              });
             },
 
             removeTodo: function (todo) {
@@ -125,6 +114,14 @@
                 return
               }
               this.editedTodo = null
+              todo.title = todo.title.trim()
+              if (!todo.title) {
+                var i = this.todos.indexOf(todo);
+                this.removeTodo(todo)
+              }
+            },
+
+            checkForDelete: function (todo) {
               todo.title = todo.title.trim()
               if (!todo.title) {
                 this.removeTodo(todo)
