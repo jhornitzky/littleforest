8b1e22180c22cb329329f0f350dea0da613f9af4
James Hornitzky <james.hornitzky@gmail.com>
2017-09-02 00:38:10 +1000

    Finally a draggable grid in Vue.js using HTML5 draggable. Now to develop the interactive bit!

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 <head>
     <title>greensquare - vege gardening made simple</title>
@@ -6,30 +7,27 @@
 </head>
 <body>
     <section class="app">
-      <header class="header" style="padding-left:6px">
+        <header class="header" style="padding-left:6px">
         <h1>green square</h1>
-      </header>
-      <section class="main container-fluid">
-          <div class="row">
-              <div class="col-3" v-for="(square,key) in squares">
-                  <div class="square">
-                      {{square.name}}
-                  </div>
-              </div>
-              <div class="col-3">
-                  <div class="square add-new" @click="addNewSquare()">
-                      + ADD
-                  </div>
-              </div>
-          </div>
-      </section>
+        </header>
+        <section class="main container-fluid">
+            <div class="row">
+                <div class="col-2" v-for="n in 36"><!-- FIXME make the max adjustable to increase plot size -->
+                    <div :data-itemid="n" class="holder" droppable="true" v-on:drop="drop" v-on:dragover="dragover">
+                        <div class="square" v-if="getsquare(n)" draggable="true" v-on:dragstart="tempdragsquare = getsquare(n)">
+                            {{getsquare(n).name}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </section>
     <footer class="info">
     </footer>
 
     <!-- scripts -->
-    <script src="js/jquery.js"></script>
-    <!--<script src="js/bootstrap.js"></script>-->
+    <!--<script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>-->
     <script src="js/vue.js"></script>
     <script src="js/vue-localstorage.js"></script>
     <script src="js/vue-draggable.js"></script>
@@ -55,15 +53,16 @@
           // app initial state
           data: {
             chart: [
-                {name:"tomato",maturityWeeks:"12"},
-                {name:"spinach",maturityWeeks:"11"},
-                {name:"lettuce",maturityWeeks:"10"},
+                {name:"tomato",maturityWeeks:"12",bestgrown:"jan-mar"},
+                {name:"spinach",maturityWeeks:"11",bestgrown:"may-jun"},
+                {name:"lettuce",maturityWeeks:"10",bestgrown:"oct-dec"},
             ],
             squares: [
-                {name:"tomato",planted:"2017-04-17"},
-                {name:"spinach",planted:"2017-04-17"},
-                {name:"lettuce",planted:"2017-04-17"}
+                {slot:1,name:"tomato",planted:"2017-04-17"},
+                {slot:5,name:"spinach",planted:"2017-04-17"},
+                {slot:10,name:"lettuce",planted:"2017-04-17"}
             ],
+            tempdragsquare:null,
             //squares: squareStorage.fetch(),
             newsquare: '',
             editedsquare: null,
@@ -86,7 +85,7 @@
           computed: {
             filteredsquares: function () {
               return this.squares
-            },
+            }
           },
 
           methods: {
@@ -103,6 +102,14 @@
               })
             },
 
+            getsquare:function(n) {
+                for (s in this.squares) {
+                    var sq = this.squares[s];
+                    if (sq.slot == n) return sq;
+                }
+                return false;
+            },
+
             removesquare: function (square) {
               this.squares.splice(this.squares.indexOf(square), 1)
             },
@@ -117,15 +124,23 @@
             },
 
             checkForDelete: function (square) {
-              square.title = square.title.trim()
-              if (!square.title) {
-                var i = this.squares.indexOf(square)
-                this.removesquare(square)
-                if (i > 0 && i < this.squares.length-1) this.focusNextTick(i)
-                else if (i > 0 && i >= this.squares.length-1) this.focusNextTick(i-1)
-              }
-            }
+                square.title = square.title.trim()
+                if (!square.title) {
+                    var i = this.squares.indexOf(square)
+                    this.removesquare(square)
+                    if (i > 0 && i < this.squares.length-1) this.focusNextTick(i)
+                    else if (i > 0 && i >= this.squares.length-1) this.focusNextTick(i-1)
+                }
+            },
 
+            drop: function(e) {
+                this.tempdragsquare.slot = e.target.getAttribute('data-itemid');
+                //allow draggable...
+            },
+
+            dragover: function(e) {
+                e.preventDefault(); //need to prevent default for draggable to work
+            }
           },
 
           // a custom directive to wait for the DOM to be updated
